250425fc286e5ea10b16d943e7c05b64abc2c644
0ri0nexe <enzo.mongin10@gmail.com>
2025-07-18 13:17:07 +0200

    fix: timout isn't fatal anymore

--- internal/source/source_linode.go
+++ internal/source/source_linode.go
@@ -1,6 +1,8 @@
 package source
 
 import (
+	"fmt"
+
 	"github.com/Escape-Technologies/cloudfinder/internal/log"
 	"github.com/Escape-Technologies/cloudfinder/pkg/provider"
 )
@@ -22,7 +24,12 @@ func (a Linode) GetIPRanges() []*IPRange {
 	ranges := make([]*IPRange, 0)
 	for _, asn := range LinodeASNs {
 		log.Info("[Linode] - Using ranges from ASN list (AS%s)", asn)
-		_ranges := getRangesForAsn(asn)
+		_ranges, err := getRangesForAsn(asn)
+		if err != nil {
+			msg := fmt.Sprintf("[Linode] - Error getting ranges for AS%s:", asn)
+			log.Error(msg, err)
+			continue
+		}
 		ranges = append(ranges, _ranges...)
 		log.Info("[Linode] - Found %d ranges for AS%s", len(ranges), asn)
 	}
--- internal/source/source_tencent.go
+++ internal/source/source_tencent.go
@@ -1,6 +1,8 @@
 package source
 
 import (
+	"fmt"
+
 	"github.com/Escape-Technologies/cloudfinder/internal/log"
 	"github.com/Escape-Technologies/cloudfinder/pkg/provider"
 )
@@ -24,7 +26,12 @@ func (a Tencent) GetIPRanges() []*IPRange {
 	ranges := make([]*IPRange, 0)
 	for _, asn := range TencentASNs {
 		log.Info("[Tencent] - Using ranges from ASN list (AS%s)", asn)
-		_ranges := getRangesForAsn(asn)
+		_ranges, err := getRangesForAsn(asn)
+		if err != nil {
+			msg := fmt.Sprintf("[Tencent] - Error getting ranges for AS%s:", asn)
+			log.Error(msg, err)
+			continue
+		}
 		ranges = append(ranges, _ranges...)
 		log.Info("[Tencent] - Found %d ranges for AS%s", len(ranges), asn)
 	}
